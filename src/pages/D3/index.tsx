dfc76e732953c8dc47cadbaac08857c03d8d1544
Wenxuan Feng <279357596@qq.com>
2021-09-28 00:40:25 +0800

    feat: react + d3 success

--- src/pages/D3/index.tsx
+++ src/pages/D3/index.tsx
@@ -1,39 +1,35 @@
 import React from "react";
 import * as d3 from 'd3';
 
-class D3Case extends React.Component<any, { name: string}> {
+class D3Case extends React.Component {
+
+    state = {
+        name: 'd3bg'
+    }
+
     constructor(props: any) {
         super(props);
 
-        this.state = {
-            name: 'd3bg'
-        }
     }
     componentDidMount() {
         const data = [5,10,15,20,25];
         console.log(data);
-        // d3.select(this.state.name)
-        //     .append('a')
-        //     .text(data.join(''));
-        d3.select('body')
+
+        // @ts-ignore
+        d3.select(this.refs.d3bg)
             .selectAll('p')
-            // .data(data)
-            // .append('p')
-            .text("new");
+            .data(data)
+            .enter()
+            .append('p')
+            .text(val => val)
+            .style('background-color', 'red')
 
     }
-
-    // componentDidMount() {
-    //     this.setState({
-    //         name: 
-    //     })
-    // }
 
     render() {
         return (
             // @ts-ignore
-            <div id={this.state.name} style={{ backgroundColor: 'red', width: 20, height: 20}} >
-                <p></p>
+            <div ref={this.state.name} >
             </div>
         );
     }
