7f01927f9be25ed4d76fc9b1a54369a67e99da69
Wenxuan Feng <279357596@qq.com>
2021-09-28 09:58:35 +0800

    feat: new column

--- src/pages/D3/index.tsx
+++ src/pages/D3/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import * as d3 from 'd3';
+import './index.css';
 
 class D3Case extends React.Component {
 
@@ -12,24 +13,40 @@ class D3Case extends React.Component {
 
     }
     componentDidMount() {
-        const data = [5,10,15,20,25];
+        // const data = [5,10,15,20,25,10,5];
+        let data: number[] = []
+        for (let i = 0; i < 25; i++) {
+            data.push(Math.random() * 30);
+        }
         console.log(data);
 
         // @ts-ignore
         d3.select(this.refs.d3bg)
-            .selectAll('p')
+            // .classed('bar', true)
+            // .selectAll('p')
+            // .data(data)
+            // .enter()
+            // .append('p')
+            // .text(val => val)
+            // .style('background-color', 'red')
+            .selectAll('div')
             .data(data)
             .enter()
-            .append('p')
-            .text(val => val)
-            .style('background-color', 'red')
+            .append('div')
+            .attr('class', 'bar')
+            // .style('display', 'inline-block')
+            // .style('background-color', 'teal')
+            // .style('width', 20 + 'px')
+            .style('height', d => d * 5 + 'px')
+            .style('margin-right', 2 + 'px')
+
 
     }
 
     render() {
         return (
             // @ts-ignore
-            <div ref={this.state.name} >
+            <div ref={this.state.name}>
             </div>
         );
     }
