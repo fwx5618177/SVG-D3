0c22d80685f79be2be578d4f89ce69b9edb90973
Wenxuan Feng <279357596@qq.com>
2021-11-09 10:08:32 +0800

    feat: new

--- src/pages/D3_4.x/component/rect/index.tsx
+++ src/pages/D3_4.x/component/rect/index.tsx
@@ -12,28 +12,65 @@ class Rect extends React.Component {
          let tmpArr = [];
          for(let i = 0; i < data.length; i++) {
              for(let j = 0; j < data.length; j++) {
+                if( data[i] === 200 && data[j] === 200) continue;
                 tmpArr.push([data[i], data[j]]);
              }
          }
 
-         return tmpArr;
+         const tmpData = [
+            [10,10, 'nw-resize'], 
+            [10,200, 'w-resize'],
+            [10,390, 'sw-resize'],
+            [200,390, 's-resize'],
+            [390,390, 'se-resize'],
+            [390,200, 'e-resize'],
+            [390,10, 'ne-resize'],
+            [200,10, 'n-resize'],
+         ]
+        //  return tmpArr;
+        return tmpData;
+     }
+
+    //  click anime
+     clickAnime() {
+         console.log(1)
+         return 0
      }
 
      componentDidMount() {
          const width = 400;
          const height = 400;
         // @ts-ignore
-         const svg = d3.select(this.refs.rect).append('svg');
-        const points = 'Z' + this.generateData(this.data).map(val => val.join(',')).join(' ');
+        const svg = d3.select(this.refs.rect).append('svg');
+        const points = this.generateData(this.data).map(val => [val[0], val[1]].join(',')).join(' ');
 
-         svg.attr('width', width)
+        svg.attr('width', width)
             .attr('height', height);
 
         svg.append('path')
-            .attr('d', points)
+            .attr('d', 'M ' + points + ' Z')
+            .attr('fill', 'none')
+            .attr('stroke', 'black')
+            .attr('stroke-width', 2)
 
         console.log(points);
-        
+
+        svg.selectAll('circle')
+        .data(this.generateData(this.data))
+        .enter()
+        .append('circle')
+        .attr('cx', d => d[0])
+        .attr('cy', d => d[1])
+        .attr('r', 10)
+        .attr('fill', 'red')
+        .attr('className', 'move')
+        .attr('style', d => `cursor: ${d[2]};`)
+        .on('onmousedown', (event) => {
+            console.log(event);
+            event.target.onmousemove = () => {
+                console.log(2);
+            }
+        })
          
      }
 
